55a96f36a6bd03580c149cb9545e93d5480dc7c5
gracecalderasitumorang <143679212+gracesitumorang17@users.noreply.github.com>
2024-02-06 21:03:28 +0700

    12S23048

--- t03_01.c
+++ t03_01.c
@@ -2,10 +2,30 @@
 
 #include <stdio.h>
 
-int main(int _argc, char **_argv)
-{
+int main(int _argc, char **_argv){
+int n = 8;
+int array [n];
 
-  // your code here
+ scanf("%d", &array[n]);
+
+for (int i = 0; i < n; i++){
+    array[i] = i;
+    scanf("%d", &array[i]);
+}
+int max = array[0];
+int min = array[0];
+for(int i = 0; i<n; i++){
+    if(array[i] > max){
+        max = array[i];
+    }
+    if(array[i] < min){
+        min = array[i];
+    }
+
+}
+
+printf("%d\n", min);
+printf("%d\n", max);
 
   return 0;
 }
