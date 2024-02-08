158e847dd1a8a685668a4251912a3c5adce81fed
gracecalderasitumorang <143679212+gracesitumorang17@users.noreply.github.com>
2024-02-09 00:15:00 +0700

    12S23048

--- t03_02.c
+++ t03_02.c
@@ -1,11 +1,41 @@
 // NIM - NAMA
 
 #include <stdio.h>
+#include <stdlib.h.>
 
 int main(int _argc, char **_argv)
 {
+    int n;
+  int array[100];
+  int sum;
 
-  // your code here
+ scanf("%d", & n);
 
+for (int i = 0; i < n; i++){
+    array[i] = i;
+    scanf("%d", &array[i]);
+}
+
+int max = array[0];
+int min = array[0];
+
+for(int i = 0; i<n; i++){
+    if(array[i] > max){
+        max = array[i];
+    }
+    if(array[i] < min){
+        min = array[i];
+    }
+
+}
+
+for(int i = 0 ; i<n; i++) {
+    sum += array [i];
+}
+float ave = (float)sum/n;
+
+printf("%d\n", min);
+printf("%d\n", max);
+printf("%.2f\n" , ave);
   return 0;
 }
